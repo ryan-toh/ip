430642d6fdd70d8641cec7471d7b23790d0e9410
ryan-toh <ryan_toh@outlook.sg>
2025-08-19 17:31:59 +0800

    Initial Skeleton Version

---
+++ src/main/java/ListGPT.java
@@ -0,0 +1,12 @@
+public class ListGPT {
+    public static void main(String[] args) {
+        final String name = "ListGPT";
+        final String linebreak = "__________________________________";
+
+        System.out.println(linebreak);
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        System.out.println(linebreak);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(linebreak);
+    }
+}
