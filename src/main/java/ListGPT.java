da976fc5c746cc6827eff6072ed6a64727f0752b
ryan-toh <ryan_toh@outlook.sg>
2025-08-27 16:27:22 +0800

    Separated ui from main method

--- src/main/java/ListGPT.java
+++ src/main/java/ListGPT.java
@@ -2,32 +2,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ListGPT {
-    private static final String name = "ListGPT";
-    private static final String linebreak = "__________________________________";
     private static TaskList list = new TaskList();
+    private static UI ui = new UI(list);
     private static Storage store = new Storage("./data/ListGPT.txt", list);
 
-    public static String message(String text) {
-        return "   " + linebreak + "\n" + text + "\n   " + linebreak + "\n";
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Retrieve before running
         store.retrieve();
 
-        System.out.println(message("   Hello! I'm " + name + "!\n   What can I do for you?"));
-
-        while (true) {
-            String input = sc.nextLine();
-            if (input.equals("bye")) {
-                break;
-            }
-            String response = Parser.parse(input, list);
-            System.out.print(message(response) + "\n");
-        }
+        ui.run();
 
         // Save on exit
         store.store();
-        System.out.println(message("   Bye. Hope to see you again soon!"));
     }
 }
---
+++ src/main/java/UI.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class UI {
+    private final String name = "ListGPT";
+    private final String linebreak = "__________________________________";
+    private TaskList list;
+
+    public UI(TaskList list) {
+        this.list = list;
+    }
+
+    public String message(String text) {
+        return "   " + linebreak + "\n" + text + "\n   " + linebreak + "\n";
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(message("   Hello! I'm " + name + "!\n   What can I do for you?"));
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                this.bye();
+                return;
+            }
+            String response = Parser.parse(input, list);
+            System.out.print(message(response) + "\n");
+        }
+    }
+
+    private void bye() {
+        System.out.println(message("   Bye. Hope to see you again soon!"));
+    }
+}
