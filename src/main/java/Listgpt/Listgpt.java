7f04fce941c6e10714427d9df7af925aaa802615
ryan-toh <ryan_toh@outlook.sg>
2025-09-09 15:20:47 +0800

    Update README

--- src/main/java/Listgpt/Listgpt.java
+++ src/main/java/Listgpt/Listgpt.java
@@ -27,6 +27,10 @@ public class Listgpt extends Application {
     private static UI userInterface = new UI(list);
     private static Storage storage = new Storage("./data/Listgpt.txt", list);
 
+    /**
+     * CLI Interface for ListGPT
+     * @param args
+     */
     public static void main(String[] args) {
         // Retrieve before running
         storage.retrieve();
@@ -37,6 +41,13 @@ public class Listgpt extends Application {
         storage.store();
     }
 
+    /**
+     * GUI Interface for ListGPT
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     */
     @Override
     public void start(Stage stage) {
         //Setting up required components
